e7583ba52b327b6dc3ffbd6260ef382ca4dbdd94
trontzal <trontzal@gmail.com>
2025-01-23 16:42:49 +0100

    Nueva entidad categoria conectada a producto

--- src/main/java/com/example/tienda/entidades/Categoria.java
+++ src/main/java/com/example/tienda/entidades/Categoria.java
@@ -1,17 +1,15 @@
 package com.example.tienda.entidades;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -26,34 +24,26 @@ import lombok.NoArgsConstructor;
 @Data
 
 @Entity
-@Table(name = "productos")
-public class Producto {
+@Table(name = "categorias")
+public class Categoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotNull
-	@NotBlank(message = "El nombre debe de ser rellenado")
+	@Column(name = "codigo", columnDefinition = "CHAR(8)")
+	private String codigo;
+	
+	@NotBlank
 	@Size(min = 3, max = 100)
 	@Column(name = "nombre")
-	@Builder.Default
-	private String nombre = "Anonimo";
+	private String nombre;
 	
-	@NotNull
-	@Min(0)
-	@Column(name = "precio")
-	private BigDecimal precio;
+	@Lob
+	@Column(name = "descripcion")
+	private String descripcion;
 	
-	@Future
-	@Column(name = "fecha_caducidad")
-	private LocalDate fechaCaducidad;
-	
-	@Min(0)
-	@Column(name = "stock")
-	private Integer stock;
-	
-	@NotNull
-	@ManyToOne
-	private Categoria categoria;
+	@OneToMany(mappedBy = "categoria")
+	private Set<Producto> productos;
 }
