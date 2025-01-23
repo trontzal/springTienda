34151227c11300ca6956fcaebe9a117d54d81f53
trontzal <trontzal@gmail.com>
2025-01-23 12:48:26 +0100

    implementacion crear producto desde admin thymeleaf

--- src/main/java/com/example/tienda/controladores/IndexController.java
+++ src/main/java/com/example/tienda/controladores/IndexController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.tienda.entidades.Producto;
+import com.example.tienda.servicios.AdminNegocio;
 import com.example.tienda.servicios.UsuarioNegocio;
 
 
@@ -17,9 +20,24 @@ public class IndexController {
 	@Autowired
 	private UsuarioNegocio usuarioNegocio;
 	
+	@Autowired
+	private AdminNegocio adminNegocio;
+	
 	@GetMapping
 	public String index(Model modelo) {
 		modelo.addAttribute("productos", usuarioNegocio.obtenerProductos());
 		return "index";
 	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+	
+	@PostMapping("/admin")
+	public String admin(Producto producto) {
+		adminNegocio.insertarProducto(producto);
+		return "redirect:/";
+	}
+	
 }
